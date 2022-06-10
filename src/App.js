41daddb5ba7fdaef4eb4c7573bbe782c0584a6f7
prateek2421 <prateekkumar3628@gmail.com>
2022-06-10 10:22:02 +0530

    New project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+
+function App(){
+  return(
+    <>
+    <Navbar/>
+    <Home/>
+    
+    
+    </>
+ 
+  );
+
+}
+
+export default App;
---
+++ src/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div style={{color:'red',display:'flex',justifyContent:'center'}}>contact</div>
+  )
+}
+
+export default Contact
